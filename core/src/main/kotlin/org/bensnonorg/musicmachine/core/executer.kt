085b5b40a0f23b619fa6135b0e96e234fef6b060
Benjamin Ye <anemailofben@gmail.com>
2019-04-11 17:04:32 -0700

    4/11 Afternoon

--- core/src/main/kotlin/org/bensnonorg/musicmachine/core/executer.kt
+++ core/src/main/kotlin/org/bensnonorg/musicmachine/core/executer.kt
@@ -2,26 +2,40 @@ package org.bensnonorg.musicmachine.core
 
 import java.util.*
 
-class DelayedExecutor {
-	var maxUpdates = 300
-	private var numUpdates = 0
-	private val queue: Queue<() -> Unit> = LinkedList()
-	fun queue(runnable: Runnable) {
-		queue.add { runnable.run() }
-	}
-
-	fun queue(block: () -> Unit) {
-		queue.add(block)
-	}
-	@JvmOverloads
-	fun run(maxUpdates: Int = this.maxUpdates) {
-		var num = 0
-		while (queue.isNotEmpty()) {
-			if (num++ < maxUpdates) queue.remove()()
+inline fun run(crossinline block: () -> Unit): Runnable {
+	return object : Runnable {
+		override fun run() {
+			block()
 		}
 	}
+}
 
-	fun run(calcUpdates: (Int) -> Int) {
-		run(calcUpdates(queue.size))
+interface QueuedExecutor {
+	fun queue(runnable: Runnable)
+	fun queue(runnable: () -> Unit)/* {
+		queue(run(runnable))
+	}*/
+	fun execute()
+}
+
+interface MappingQueuedExecutor : QueuedExecutor
+open class ScalableBatchExecutor : QueuedExecutor {
+	override fun queue(runnable: () -> Unit) {
+		queue(run(runnable))
+	}
+
+	private var numUpdates = 0
+	private val queue: Queue<Runnable> = LinkedList()
+	override fun queue(runnable: Runnable) {
+		queue.add(runnable)
+	}
+
+	override fun execute() {
+		var num = 0
+		while (queue.isNotEmpty()) {
+			if (num++ < numUpdates) {
+				queue.remove().run()
+			}
+		}
 	}
 }
